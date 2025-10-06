6e66c77dd77f92d1aaf23ebbcb81e30d4d5b25d8
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2025-10-06 20:27:57 +0200

    feat: put functions signature up top and crew member listing now works

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int handleCmdInput(char* cmd)
+enum job {
+    ENGINEER,
+};
+
+typedef struct
 {
-    if (strcmp(cmd, "crew ls\n") == 0)
-    {
-        printf("Crew List !\n");
-        return 1;
-    }
-    
-    return -1;
-}
+    char name[30];
+    enum job job;
+} crewMember;
+
+crewMember** crewList;
+size_t crewListSize = 0;
+
+int initCrew(int crewSize);
+int handleCmdInput(char* cmd);
+int addCrewMember(crewMember *newCrewMember);
+int listCrew();
 
 int main() 
 {
+    initCrew(4);
+    
     while (1 == 1)
     {
         char cmd[20];
@@ -26,5 +36,69 @@ int main()
         
         handleCmdInput(cmdPtr);
     }
+}
+
+
+int handleCmdInput(char* cmd)
+{
+    if (strcmp(cmd, "crew ls\n") == 0)
+    {
+        listCrew();
+        return 0;
+    }
     
+    return -1;
+}
+
+int addCrewMember(crewMember *newCrewMember)
+{
+    crewMember **newCrewList = malloc(sizeof(crewMember*) * (crewListSize + 1));
+    for (size_t i = 0; i < crewListSize; i++)
+    {
+        newCrewList[i] = crewList[i];
+    }
+
+    newCrewList[crewListSize] = newCrewMember;
+    crewListSize++;
+
+    free(crewList);
+
+    crewList = newCrewList;
+    return 0;
+}
+
+int initCrew(int crewSize)
+{
+    crewMember *crewMember_1 = malloc(sizeof(crewMember));
+    strcpy(crewMember_1->name, "James");
+    crewMember_1->job = ENGINEER;
+    addCrewMember(crewMember_1);
+}
+
+int listCrew() {
+    printf("Crew :\n");
+
+    if (crewListSize == 0)
+    {
+        printf("The crew is empty.\n");
+        return 0;
+    }
+    
+    
+    for (size_t i = 0; i < crewListSize; i++)
+    {
+        printf("Name: %s\n", crewList[i]->name);
+        switch ( crewList[i]->job )
+        {
+        case ENGINEER:
+            printf("Job: Engineer\n");
+            break;
+        
+        default:
+            printf("Job: Unregistered Job\n");
+            break;
+        }
+    }
+    
+    return 0;
 }
