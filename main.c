1ebce2b084697bf9e7490be3dcae9942f1673ee2
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2025-10-06 14:29:14 +0200

    do something if the command is right

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+int handleCmdInput(char* cmd)
+{
+    if (strcmp(cmd, "crew ls\n") == 0)
+    {
+        printf("Crew List !\n");
+        return 1;
+    }
+    
+    return -1;
+}
 
 int main() 
 {
+    while (1 == 1)
+    {
+        char cmd[20];
+        printf("Enter a command :\n");
+        fgets(cmd, 20, stdin); 
 
+        printf("You've entered : %s\n", cmd);
+
+        char* cmdPtr = cmd;
+        
+        handleCmdInput(cmdPtr);
+    }
+    
 }
-
